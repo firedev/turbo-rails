388c95526b49fb40e4ab0555f450b6b414a2b774
Nikolay <nick@firedev.com>
2022-05-24 19:17:11 +0700

    quotes controller

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,45 @@
+class QuotesController < ApplicationController
+  before_action :set_quote, only: %i[show edit update destroy]
+
+  def index
+    @quotes = Quote.all
+  end
+
+  def show
+    de
+  end
+
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+    if @quote.save
+      redirect_to quotes_path, notice: 'Great success'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @quote.update(quote_params)
+      redirect_to quotes_path, notice: 'Great update'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @quote.destroy
+    redirect_to quotes_path, notice: 'Wasted'
+  end
+
+  private
+
+  def set_quote
+    @quote = Quote.find(params[:id])
+  end
+end
