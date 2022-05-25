d0d1420dd37e448c3ada7954f97c232cfb3090b6
Nikolay <nick@firedev.com>
2022-05-25 11:30:30 +0700

    Seed

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -17,7 +17,7 @@ class QuotesController < ApplicationController
     if @quote.save
       redirect_to quotes_path, notice: 'Great success'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -27,7 +27,7 @@ class QuotesController < ApplicationController
     if @quote.update(quote_params)
       redirect_to quotes_path, notice: 'Great update'
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
