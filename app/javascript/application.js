50f66dac78b0e224b866d8a9f3ad00c3c65f01db
Nikolay <nick@firedev.com>
2022-05-31 14:06:17 +0700

    progress bar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,11 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
-// import { Turbo } from "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 // Turbo.session.drive = false
+document.addEventListener("DOMContentLoaded", () => {
+  // The default of 500ms is too long and
+  // users can lose the causal link between clicking
+  // a link and seeing the browser respond
+  Turbo.setProgressBarDelay(100)
+})
